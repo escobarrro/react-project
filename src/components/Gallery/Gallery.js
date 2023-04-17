0f98aa62ea780c57dd2484f5f2fd079712e3d31b
escobarrro <pblplrz@gmail.com>
2023-04-18 01:03:10 +0200

    changes

---
+++ src/components/Gallery/Gallery.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Gallery.css';
+
+function Gallery()  {
+
+    return (
+        <div>
+            <h1>Galeria</h1>
+        <div class="container">
+  <div class="gallery">
+    <img src="https://picsum.photos/300/200?random=1" alt="Zdjęcie 1" />
+    <img src="https://picsum.photos/300/200?random=2" alt="Zdjęcie 2"/>
+    <img src="https://picsum.photos/300/200?random=3" alt="Zdjęcie 3"/>
+    <img src="https://picsum.photos/300/200?random=4" alt="Zdjęcie 4"/>
+    <img src="https://picsum.photos/300/200?random=5" alt="Zdjęcie 5"/>
+    <img src="https://picsum.photos/300/200?random=6" alt="Zdjęcie 6"/>
+    <img src="https://picsum.photos/300/200?random=7" alt="Zdjęcie 7"/>
+    <img src="https://picsum.photos/300/200?random=8" alt="Zdjęcie 8"/>
+    <img src="https://picsum.photos/300/200?random=9" alt="Zdjęcie 9"/>
+    <img src="https://picsum.photos/300/200?random=10" alt="Zdjęcie 10"/>
+  </div>
+</div>
+</div>
+    );
+    }
+
+
+export default Gallery;
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './Sidebar.css';
-import { BulbOutline } from 'react-ionicons'
-import { CodeOutline } from 'react-ionicons'
-import { ImagesOutline } from 'react-ionicons'
+import Gallery from '../Gallery/Gallery.js';
+import { BulbOutline } from 'react-ionicons';
+import { CodeOutline } from 'react-ionicons';
+import { ImagesOutline } from 'react-ionicons';
 
 
 class Sidebar extends React.Component {
@@ -11,21 +12,9 @@ class Sidebar extends React.Component {
             <div id="sidebar">
                 <ul id="list">
                 <li onClick={() => this.props.handleClick(
-            <div style={{padding: "10px", lineHeight: "1.7"}}>
+            <div style={{padding: "10px", lineHeight: "2"}}>
                 <h1 style={{textAlign: "center", fontSize: "35px"}}>Zajawka do Front-endu</h1>
-                <p style={{ textAlign: "justify", fontSize: "17px" }}>Moja zajawka do Front-endu pojawiła się gdzieś w okolicach Października 2022, kiedy na praktykach zainteresowałem się bardziej Front-endem i ogólnie Web develomentem. Zacząłem pisać swoje pierwsze podstawowe strony ulepszając je o nowo poznane rzeczy, nie były one jakoś bardzo skomplikowane i czułem że czegoś mi brakuje. Dlatego po paru dniach kupiłem kurs który miał mnie rozwinąć w całym zakresie Web Developmentu począwszy od przypomnienia podstaw HTML, przez CSS, JS aż do poznania biblioteki JS jaką jest React, jeśli chcesz się dowiedzieć więcej na temat tej biblioteki kliknij w 4 od góry przycisk na bocznym panelu. Wracając po nauce podstaw Reacta, React Hooks oraz Redux przeszedłem do poznania podstaw Back-endu która opierała się na Node.js oraz Express.js. Poznałem również jak działają i jak korzystać z baz danych, API itp. dodatkowo poznałem jak działa GitHub jak z niego korzystać, jak wygląda dzień Web Developera i pare porad jak zacząc szukać pracy i jak dalej polepszać swoje umiejętności. Na sam koniec kursu czekał na mnie wielki projekt, zająć się musiałem Front-endem jak i Back-endem nie było to wszystko takie proste ale dodało dużo nowych umiejętności, informacji a przedewszystkim zwiększało doświadczenie, które jest bardzo ważne. Poniżej zamieszcze kilka moich prac, wraz z kodem.</p>
-                <h2 style={{textAlign: "center"}}>Moje przykładowe prace</h2>
-                <div style={{display: "grid", gridTemplateColumns: "repeat(3, 1fr)", gridGap: "20px", textAlign: "center", marginTop: "50px"}}>
-                    <div>
-                    <a target="_blank" rel="noreferrer" href="https://www.google.com/"><img src="https://picsum.photos/300/200?random=1" alt="" /></a>
-                    </div>
-                    <div class="item">
-			        <a target="_blank" rel="noreferrer" href="https://www.yahoo.com/"><img src="https://picsum.photos/300/200?random=2" alt="" /></a>
-		            </div>
-		            <div class="item">
-			        <a target="_blank" rel="noreferrer" href="https://www.bing.com/"><img src="https://picsum.photos/300/200?random=3" alt="" /></a>
-		            </div>
-                </div>
+                <p style={{ textAlign: "justify", fontSize: "20px" }}>Moja zajawka do Front-endu pojawiła się gdzieś w okolicach Października 2022, kiedy na praktykach zainteresowałem się bardziej Front-endem i ogólnie Web develomentem. Zacząłem pisać swoje pierwsze podstawowe strony ulepszając je o nowo poznane rzeczy, nie były one jakoś bardzo skomplikowane i czułem że czegoś mi brakuje. Dlatego po paru dniach kupiłem kurs który miał mnie rozwinąć w całym zakresie Web Developmentu począwszy od przypomnienia podstaw HTML, przez CSS, JS aż do poznania biblioteki JS jaką jest React, jeśli chcesz się dowiedzieć więcej na temat tej biblioteki kliknij w 4 od góry przycisk na bocznym panelu. Wracając po nauce podstaw Reacta, React Hooks oraz Redux przeszedłem do poznania podstaw Back-endu która opierała się na Node.js oraz Express.js. Poznałem również jak działają i jak korzystać z baz danych, API itp. dodatkowo poznałem jak działa GitHub jak z niego korzystać, jak wygląda dzień Web Developera i pare porad jak zacząc szukać pracy i jak dalej polepszać swoje umiejętności. Na sam koniec kursu czekał na mnie wielki projekt, zająć się musiałem Front-endem jak i Back-endem nie było to wszystko takie proste ale dodało dużo nowych umiejętności, informacji a przedewszystkim zwiększało doświadczenie, które jest bardzo ważne. Poniżej zamieszcze kilka moich prac, wraz z kodem.</p>
             </div>
             )}>
             <BulbOutline
@@ -44,7 +33,9 @@ class Sidebar extends React.Component {
                     <li style={{ fontSize: "17px" }}>Edytory kodu, takie jak Visual Studio Code, Sublime Text czy Atom, które oferują przyjazne środowiska pracy dla programistów front-endowych</li>
                     </ul>
                     <p style={{ textAlign: "justify", fontSize: "17px" }}>Podsumowując w dzisiejszych czasach rola front-end developera staje się coraz ważniejsza, ponieważ wzrasta liczba urządzeń, na których korzystamy z internetu (np. smartfony, tablety, komputery) oraz coraz bardziej rozbudowane wymagania dotyczące interaktywności i estetyki stron internetowych.</p>
+                    /
                 </div>
+
             )}>
             <CodeOutline
     color={'#00000'} 
@@ -83,7 +74,9 @@ class Sidebar extends React.Component {
           >
             <path d="M12 10.11c1.03 0 1.87.84 1.87 1.89 0 1-.84 1.85-1.87 1.85-1.03 0-1.87-.85-1.87-1.85 0-1.05.84-1.89 1.87-1.89M7.37 20c.63.38 2.01-.2 3.6-1.7-.52-.59-1.03-1.23-1.51-1.9a22.7 22.7 0 01-2.4-.36c-.51 2.14-.32 3.61.31 3.96m.71-5.74l-.29-.51c-.11.29-.22.58-.29.86.27.06.57.11.88.16l-.3-.51m6.54-.76l.81-1.5-.81-1.5c-.3-.53-.62-1-.91-1.47C13.17 9 12.6 9 12 9c-.6 0-1.17 0-1.71.03-.29.47-.61.94-.91 1.47L8.57 12l.81 1.5c.3.53.62 1 .91 1.47.54.03 1.11.03 1.71.03.6 0 1.17 0 1.71-.03.29-.47.61-.94.91-1.47M12 6.78c-.19.22-.39.45-.59.72h1.18c-.2-.27-.4-.5-.59-.72m0 10.44c.19-.22.39-.45.59-.72h-1.18c.2.27.4.5.59.72M16.62 4c-.62-.38-2 .2-3.59 1.7.52.59 1.03 1.23 1.51 1.9.82.08 1.63.2 2.4.36.51-2.14.32-3.61-.32-3.96m-.7 5.74l.29.51c.11-.29.22-.58.29-.86-.27-.06-.57-.11-.88-.16l.3.51m1.45-7.05c1.47.84 1.63 3.05 1.01 5.63 2.54.75 4.37 1.99 4.37 3.68 0 1.69-1.83 2.93-4.37 3.68.62 2.58.46 4.79-1.01 5.63-1.46.84-3.45-.12-5.37-1.95-1.92 1.83-3.91 2.79-5.38 1.95-1.46-.84-1.62-3.05-1-5.63-2.54-.75-4.37-1.99-4.37-3.68 0-1.69 1.83-2.93 4.37-3.68-.62-2.58-.46-4.79 1-5.63 1.47-.84 3.46.12 5.38 1.95 1.92-1.83 3.91-2.79 5.37-1.95M17.08 12c.34.75.64 1.5.89 2.26 2.1-.63 3.28-1.53 3.28-2.26 0-.73-1.18-1.63-3.28-2.26-.25.76-.55 1.51-.89 2.26M6.92 12c-.34-.75-.64-1.5-.89-2.26-2.1.63-3.28 1.53-3.28 2.26 0 .73 1.18 1.63 3.28 2.26.25-.76.55-1.51.89-2.26m9 2.26l-.3.51c.31-.05.61-.1.88-.16-.07-.28-.18-.57-.29-.86l-.29.51m-2.89 4.04c1.59 1.5 2.97 2.08 3.59 1.7.64-.35.83-1.82.32-3.96-.77.16-1.58.28-2.4.36-.48.67-.99 1.31-1.51 1.9M8.08 9.74l.3-.51c-.31.05-.61.1-.88.16.07.28.18.57.29.86l.29-.51m2.89-4.04C9.38 4.2 8 3.62 7.37 4c-.63.35-.82 1.82-.31 3.96a22.7 22.7 0 012.4-.36c.48-.67.99-1.31 1.51-1.9z" />
           </svg></li>
-          <li onClick={() => this.props.handleClick("Tekst do przycisku 5")}>
+          <li onClick={() => this.props.handleClick(
+            <Gallery />
+          )}>
             <ImagesOutline
     color={'#00000'} 
     height="50px"
