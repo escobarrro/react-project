fc672d28c989ac18d3f05800b6d964cac77d67ce
escobarrro <pblplrz@gmail.com>
2023-04-17 22:15:47 +0200

    change

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://git.heroku.com/sheltered-shelf-07975.git/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
